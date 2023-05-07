c54015aa7577c8ff8958909eb224b5317da4eed6
benben121212 <benoitdelpontnew@gmail.com>
2023-05-07 20:33:16 +0200

    Bootstrap

---
+++ Assets/js/scripts.js
@@ -0,0 +1,11 @@
+// Récupérer l'élément d'entrée de l'email
+var emailInput = document.getElementById("exampleFormControlInput1");
+
+// Récupérer la valeur de l'entrée de l'email
+var emailValue = emailInput.value;
+
+// Récupérer l'élément de la zone de texte
+var textarea = document.getElementById("exampleFormControlTextarea1");
+
+// Récupérer la valeur de la zone de texte
+var textareaValue = textarea.value;
